56c60914cb032024cdd6cdec4d18c5e5f1dc9489
Murilo Santana <mvrilo@gmail.com>
2015-08-15 12:29:49 -0300

    check without typing the code, add command to store the code in a file

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # correios
 
 Track your shit from [correios](http://www.correios.com.br/) via command line.
+
+# Usage
+
+Track one or more orders:
+
+```
+correios check [code1] [code2]
+```
+
+# Author
+
+Murilo Santana <<mvrilo@gmail.com>>
+
+# License
+
+MIT
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -15,6 +17,10 @@ const (
 	detailedURL = "http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm"
 )
 
+var db = &database{
+	dir: os.Getenv("HOME") + "/.correios",
+}
+
 type result struct {
 	codes  string
 	orders []order
@@ -24,6 +30,70 @@ type order struct {
 	id     string
 	status string
 	date   string
+}
+
+type database struct {
+	*os.File
+	dir string
+}
+
+func (d *database) open() (f *os.File, err error) {
+	if d.File != nil {
+		return d.File, nil
+	}
+	if f, err = os.OpenFile(d.dir, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644); err != nil {
+		return
+	}
+	d.File = f
+	return
+}
+
+func (d *database) scan(fn func(*bufio.Scanner)) {
+	f, err := d.open()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		fn(s)
+	}
+
+	if err = s.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (d *database) codeExists(code string) (b bool) {
+	d.scan(func(s *bufio.Scanner) {
+		b = strings.Contains(s.Text(), code)
+	})
+	return b
+}
+
+func (d *database) read() (data []string) {
+	d.scan(func(s *bufio.Scanner) {
+		a := s.Text()
+		data = append(data, a)
+	})
+	return data
+}
+
+func (d *database) write(code string) (bool, error) {
+	if d.codeExists(code) {
+		return true, nil
+	}
+
+	f, err := d.open()
+	if err != nil {
+		return false, err
+	}
+
+	if _, err = f.WriteString(fmt.Sprintf("%s\n", code)); err != nil {
+		return false, err
+	}
+	return false, nil
 }
 
 func fetchOrders(codes string) *result {
@@ -86,24 +156,59 @@ func (r *result) String() (ret string) {
 	return
 }
 
+func check(cmd *cobra.Command, args []string) {
+	var codes string
+	if len(args) > 0 {
+		codes = strings.Join(args, ";")
+	} else {
+		for i, line := range db.read() {
+			if i > 0 {
+				codes += ";"
+			}
+			codes += strings.Split(line, " ")[0]
+		}
+	}
+	res := fetchOrders(codes)
+	fmt.Println(res)
+}
+
+func add(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		cmd.Usage()
+		return
+	}
+
+	exists, err := db.write(args[0])
+	if exists {
+		fmt.Println("code already added")
+		return
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
 func main() {
 	correios := &cobra.Command{
 		Use:  "correios",
 		Long: "Simple command line tool to track your orders from Correios",
 	}
-	checker := &cobra.Command{
-		Use:   "check [codes]",
-		Short: "Check the status of one or more orders",
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 0 {
-				fmt.Println(cmd.Usage())
-				return
-			}
-			res := fetchOrders(strings.Join(args, ";"))
-			fmt.Println(res)
-		},
+	checkCmd := &cobra.Command{
+		Use:   "check [code] [code]",
+		Short: "Check the status of one or more orders or the ones that you previously added",
+		Run:   check,
+	}
+	addCmd := &cobra.Command{
+		Use:   "add <code> [name]",
+		Short: "Store a order code to check later without specifying it",
+		Long: ` After adding a order, simply just check the orders without passing the code to check command
+e.g.
+$ correios check`,
+		Run: add,
 	}
 
-	correios.AddCommand(checker)
+	correios.AddCommand(checkCmd)
+	correios.AddCommand(addCmd)
 	correios.Execute()
 }
